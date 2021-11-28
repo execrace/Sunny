6ee79301fe86652d0b5c8c1171213798e717f48e
ssh <ssh>
2021-11-28 12:25:12 +0400

    Initial Buttons

--- Sunny/Sunny/ViewController.swift
+++ Sunny/Sunny/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+    
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
