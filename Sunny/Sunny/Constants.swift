2d349293543ed5d4ac82eb6db01e905b4a443f2d
ssh <ssh>
2021-11-29 21:24:44 +0400

    Request from network

---
+++ Sunny/Sunny/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Sergey Shirmanov on 29.11.2021.
+//
+
+import Foundation
+
+let apiKey = "b072ea3c5443cf6c3599c856e3faa937"
--- Sunny/Sunny/ViewController.swift
+++ Sunny/Sunny/ViewController.swift
@@ -14,25 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
     
+    let networkWeatherManager = NetworkWeatherManager()
     
     @IBAction func searchPressed(_ sender: UIButton) {
-    
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        networkWeatherManager.fetchCurrentWeather(forCity: "London")
     }
-
-
 }
 
