270da21db359fa24a3a1e3be58d37cbc07e46dab
ssh <ssh>
2021-11-30 20:39:26 +0400

    Parse JSON

---
+++ Sunny/Sunny/CurrentWeather.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentWeather.swift
+//  Sunny
+//
+//  Created by Sergey Shirmanov on 30.11.2021.
+//
+
+import Foundation
---
+++ Sunny/Sunny/CurrentWeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentWeatherData.swift
+//  Sunny
+//
+//  Created by Sergey Shirmanov on 30.11.2021.
+//
+
+import Foundation
