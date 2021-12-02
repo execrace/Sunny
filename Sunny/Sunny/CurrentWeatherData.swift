0e704be8f50dcbc2812898344b7048c423ea4597
ssh <ssh>
2021-12-02 19:38:15 +0400

    Parse JSON

--- Sunny/Sunny/CurrentWeatherData.swift
+++ Sunny/Sunny/CurrentWeatherData.swift
@@ -2,7 +2,27 @@
 //  CurrentWeatherData.swift
 //  Sunny
 //
-//  Created by Sergey Shirmanov on 30.11.2021.
+//  Created by Sergey Shirmanov on 01.12.2021.
 //
 
 import Foundation
+
+struct CurrentWeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let  temp: Double
+    let feelsLlike: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLlike = "feels_like"
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+}
