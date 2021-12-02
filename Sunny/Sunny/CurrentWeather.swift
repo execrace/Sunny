b96c851899e4a85e26e36d0780bdd5a65daff58e
ssh <ssh>
2021-12-02 20:01:08 +0400

    Parse JSON one more time

--- Sunny/Sunny/CurrentWeather.swift
+++ Sunny/Sunny/CurrentWeather.swift
@@ -2,7 +2,31 @@
 //  CurrentWeather.swift
 //  Sunny
 //
-//  Created by Sergey Shirmanov on 30.11.2021.
+//  Created by Sergey Shirmanov on 02.12.2021.
 //
 
 import Foundation
+
+struct CurrentWeather {
+    let cityName: String
+    
+    let temperature: Double
+    var temperatureString: String {
+        return "\(temperature.rounded())"
+    }
+    
+    let feelsLikeTemperature: Double
+    var feelsLikeTemperatureString: String {
+        return "\(feelsLikeTemperature.rounded())"
+    }
+    
+    let conditionCode: Int
+
+init?(currentWeatherData: CurrentWeatherData) {
+    cityName = currentWeatherData.name
+    temperature = currentWeatherData.main.temp
+    feelsLikeTemperature = currentWeatherData.main.feelsLlike
+    conditionCode = currentWeatherData.weather.first!.id
+    }
+}
+
